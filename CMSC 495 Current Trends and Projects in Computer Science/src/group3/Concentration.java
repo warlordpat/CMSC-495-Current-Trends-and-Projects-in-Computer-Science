f61bfb35577400d6ba697b7a15881a76993adcf3
Patrick Smith <warlordpat@hotmail.com>
2016-09-05 19:51:46 -0400

    Fixed timing of second card turn-over

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/Concentration.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/Concentration.java
@@ -21,7 +21,7 @@ public class Concentration extends JFrame implements ActionListener {
     int selectedCard = 0;
     int secondSelectedCard = 0;
     int index = 0;
-    Timer t = new Timer (2000,this); 
+    Timer t = new Timer(2000, this);
     JButton btn1 = new JButton(cardBacks);
     JButton btn2 = new JButton(cardBacks);
     JButton btn3 = new JButton(cardBacks);
@@ -48,122 +48,124 @@ public class Concentration extends JFrame implements ActionListener {
     JButton btn24 = new JButton(cardBacks);
     JButton btn25 = new JButton(cardBacks);
     JButton btn26 = new JButton(cardBacks);
-    JButton[] buttons = {btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17,
-                            btn18, btn19, btn20, btn21, btn22, btn23, btn24, btn25, btn26};
+    JButton[] buttons = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14,
+            btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23, btn24, btn25, btn26 };
 
-    
-    
     public Concentration() {
-                 
-//        createGUI();
-                
-    }
-    
-    public static void main(String[] args) {          
-        
-       new Concentration();
-        
-    }
-//    private void delay() {
-//    
-//        
-//         t.start();
-//         t.setRepeats(false);
-//        
-//    }
-     @Override
-        public void actionPerformed(ActionEvent e){
-                       
-            index = Arrays.asList(buttons).indexOf(e.getSource());
-           
-            
-            System.out.println("test" + test);
 
-            buttons[index].setIcon(cards[index]);
-            t.start();
-            t.stop();
-            checkForMatch();
-        
+        // createGUI();
+
     }
-        
+
+    public static void main(String[] args) {
+
+        new Concentration();
+
+    }
+
+    // private void delay() {
+    //
+    //
+    // t.start();
+    // t.setRepeats(false);
+    //
+    // }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        index = Arrays.asList(buttons).indexOf(e.getSource());
+
+        System.out.println("test" + test);
+
+        buttons[index].setIcon(cards[index]);
+        t.start();
+        t.stop();
+        checkForMatch();
+
+    }
+
     public void checkForMatch() {
-                 
-            System.out.println("index" + index);
-            if (test == 0){
-                    
-                selectedCard = index; 
-               
-            }
-            else {
-                
-                secondSelectedCard = index;
-                
-                //buttons[secondSelectedCard].setIcon(cards[secondSelectedCard] );
-                //delay();
-            }
-	    
-System.out.println("selected" + selectedCard);
+
+        System.out.println("index" + index);
+        if (test == 0) {
+
+            selectedCard = index;
+
+        } else {
+
+            secondSelectedCard = index;
+            
+            // buttons[secondSelectedCard].setIcon(cards[secondSelectedCard] );
+            // delay();
+        }
+
+        System.out.println("selected" + selectedCard);
 
         test += 1;
-             
-        
+
         if (test == 2) {
-       
-        System.out.println("second:" + cards[secondSelectedCard]);
-        System.out.println("first" + cards[selectedCard]);
-         
-            if (cards[secondSelectedCard].toString() == null ? cards[selectedCard].toString() == null : cards[secondSelectedCard].toString().equals(cards[selectedCard].toString())) {
+
+            System.out.println("second:" + cards[secondSelectedCard]);
+            System.out.println("first" + cards[selectedCard]);
+
+            if (cards[secondSelectedCard].toString() == null ? cards[selectedCard].toString() == null
+                    : cards[secondSelectedCard].toString().equals(cards[selectedCard].toString())) {
                 if (selectedCard != secondSelectedCard) {
 
                     buttons[selectedCard].setEnabled(false);
                     buttons[secondSelectedCard].setEnabled(false);
-                }
-                else {
+                } else {
                     buttons[selectedCard].setIcon(cardBacks);
                 }
-                test = 0; 
-            } 
-            else {
-
-                buttons[selectedCard].setIcon(cardBacks);
-                buttons[secondSelectedCard].setIcon(cardBacks);
                 test = 0;
+            } else {
+                SwingUtilities.invokeLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        }
+                        buttons[selectedCard].setIcon(cardBacks);
+                        buttons[secondSelectedCard].setIcon(cardBacks);
+                        test = 0;
+                    }
+                });
             }
-        }        
+        }
     }
-    
+
     public void createGUI() {
-        
+
         for (int i = 1; i <= 2; i++) {
             for (int j = 1; j < 14; j++) {
-            
+
                 int position = 0;
                 do {
-                    position = (int)(Math.random()*26);
-                }
-                while (cards[position] != null);
+                    position = (int) (Math.random() * 26);
+                } while (cards[position] != null);
                 /// look at cardURL
                 URL cardURL = this.getClass().getClassLoader().getResource(j + ".png");
                 cards[position] = new ImageIcon(cardURL);
-            
-            }  
+
+            }
         }
-        
-       
+
         JFrame frame = new JFrame("Concentration!");
         frame.setVisible(true);
         frame.setSize(900, 900);
         frame.setResizable(false);
-        JPanel panel = new JPanel(new GridLayout(5,6));
-                
-        
-        for (int i = 0; i <26; i++) {
-            
+        JPanel panel = new JPanel(new GridLayout(5, 6));
+
+        for (int i = 0; i < 26; i++) {
+
             buttons[i].addActionListener(this);
-            
+
             panel.add(buttons[i]);
 
-        }  
+        }
 
         frame.add(panel);
         frame.pack();
