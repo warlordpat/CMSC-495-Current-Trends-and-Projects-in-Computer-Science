f471f2b52bbc01dfc115f17e38fbde97099c4a66
Patrick Smith <warlordpat@hotmail.com>
2016-09-05 21:26:17 -0400

    Fixed some display sizes.  Initial Deal working.

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/Blackjack.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/Blackjack.java
@@ -3,22 +3,30 @@ package group3;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.util.*;
 import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.plaf.synth.SynthSeparatorUI;
 
+/**
+ * @author Patrick Smith
+ * @version 1.0
+ * @since Sep 5, 2016
+ */
 public class Blackjack extends JPanel {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 510809952979347694L;
     private Deck deck;
     private Hand dealer, player;
     boolean handInPlay;
 
+    /**
+     * 
+     */
     public Blackjack() {
         setLayout(null);
         setBackground(new Color(7, 99, 36)); // Card Table Green
-        
+
     }
 
     public void begin() {
@@ -31,19 +39,20 @@ public class Blackjack extends JPanel {
     void newGame() {
         deck = new Deck();
         deck.shuffle();
-        deck.setBounds(60, 35, 79, 123); // set the deck size
+        deck.setBounds(60, 35, 79, 96); // set the deck size
         System.out.println("Adding Deck");
         add(deck); // add the deck to the play surface
-        
+
         System.out.println("Adding Dealer");
         dealer = new Hand();
-        dealer.setBounds(400, 100, (2 * 79), 123);
+        dealer.setBounds(400, 100, (2 * 79), 96);
         add(dealer);
         player = new Hand();
-        
+
         handInPlay = false;
         repaint();
     }
+
     public String display() {
         String output = "";
         if (handInPlay) {
@@ -70,6 +79,10 @@ public class Blackjack extends JPanel {
         return output;
     }
 
+    /**
+     * Deals out initial hand in BlackJack. Shuffles the deck when the number of
+     * Cards left gets low.
+     */
     public void deal() {
         if (deck.deckSize() < 8) {
             deck = new Deck();
@@ -80,28 +93,45 @@ public class Blackjack extends JPanel {
             System.out.println("Dealing new hand");
             remove(dealer);
             dealer = new Hand();
-            dealer.setBounds(400, 100, (2* 79), 123); // adds a new dealer hand to the GUI
+            dealer.setBounds(400, 100, (2 * 72), 96); // adds a new dealer hand
+                                                      // to the GUI
             add(dealer);
-            
+
             player = new Hand();
-            player.setBounds(400, 400, (2* 79), 123); // adds a new dealer hand to the GUI
+            player.setBounds(400, 400, (2 * 72), 96); // adds a new player hand
+                                                      // to the GUI
             add(player);
-            
+
             Card temp = deck.deal();
-            temp.flip();
             player.addCard(temp);
-            player.addCard(deck.deal());
+            temp.flip();
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            
-            dealer.addCard(deck.deal());
+            temp = deck.deal();
+            dealer.addCard(temp);
+            temp.flip();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            temp = deck.deal();
+            player.addCard(temp);
+            temp.flip();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             dealer.addCard(deck.deal());
             System.out.println(dealer);
-            temp.flip();
             handInPlay = true;
             repaint();
         }
@@ -135,6 +165,9 @@ public class Blackjack extends JPanel {
         return handInPlay;
     }
 
+    /**
+     * New BlackJack GUI
+     */
     public void patrickGUI() {
         JFrame frame = new JFrame("Black Jack");
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -188,6 +221,11 @@ public class Blackjack extends JPanel {
         frame.setSize(600, 600);
     }
 
+    /**
+     * Runs a test of BlackJack by itself.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Blackjack blackjack = new Blackjack();
         blackjack.begin();
