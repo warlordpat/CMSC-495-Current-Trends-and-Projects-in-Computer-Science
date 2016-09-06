f8382d320bc78db762c8563d151993ed22bddd9c
Patrick Smith <warlordpat@hotmail.com>
2016-09-05 20:03:26 -0400

    Updated comments and removed old code.

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/Card.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/Card.java
@@ -4,19 +4,25 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 
 /**
+ * The Card class represents a playing card. It extends JComponent so it can be
+ * displayed directly to the screen. It offers the ability to query its suit,
+ * rank, and allows the user to flip the card over. Card images are loaded from
+ * a sprite map statically to save memory and speed up card creation.
+ * 
  * @author Patrick Smith
- * @version 1.0
+ * @version 1.1
  * @since Sep 5, 2016
  */
 public class Card extends JComponent {
 
+    /**
+     * Generated serial ID.
+     */
+    private static final long serialVersionUID = -2835610009194889206L;
     /**
      * Statically load the cards, so the card sprite map is only in memory once.
      */
@@ -38,7 +44,6 @@ public class Card extends JComponent {
         try {
             return ImageIO.read(Card.class.getClassLoader().getResource(location));
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             System.out.println("Error in image load from Card Class!");
             e.printStackTrace();
             throw new Error(e);
@@ -53,36 +58,40 @@ public class Card extends JComponent {
      * The suit of the card.
      */
     private Suit suit;
+    /**
+     * If the card is face up or face down.
+     */
     boolean faceUp;
     // URL imageURL;
     // ImageIcon cardBacks;
+    /**
+     * The image of the back of the card.
+     */
     BufferedImage back;
+    /**
+     * The image of the front of the card.
+     */
     BufferedImage front;
+    /**
+     * The image currently displayed on screen.
+     */
     BufferedImage currentImage;
 
     /**
-     * Creates a Card of the given rank and suit.
+     * Creates a Card of the given rank and suit and loads the image for the
+     * card.
+     * 
+     * @param rank
+     *            the rank of the Card
+     * @param suit
+     *            the suit of the Card
      */
     public Card(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
-        // imageURL =
-        // getClass().getClassLoader().getResource("images/card_backs.png");
-        // cardBacks = new ImageIcon(imageURL);
-        // int suitRow = suit.ordinal() + 1;
-        // int rankCol = rank.ordinal() + 1;
-        // URL cardURL = getClass().getClassLoader().getResource("images/" +
-        // suitRow * rankCol + ".png");
-        // URL cardURL =
-        // getClass().getClassLoader().getResource("images/cards_2.png");
-        // System.out.println("loading " + suitRow * rankCol + ".png");
         System.out.println("loading " + rank.ordinal() * 72 + ", " + suit.ordinal() * 96 + "");
         back = cardBacks.getSubimage(72, 0, 72, 96);
         front = cards.getSubimage(rank.ordinal() * 72, suit.ordinal() * 96, 72, 96);
-        // System.out.println("back: " + back.getWidth() + ", " +
-        // back.getHeight());
-        // System.out.println("front: " + front.getWidth() + ", " +
-        // front.getHeight());
         currentImage = back;
         faceUp = false;
     } // end constructor
@@ -105,6 +114,9 @@ public class Card extends JComponent {
         return rank;
     } // end method
 
+    /**
+     * Flips the card over on screen.
+     */
     void flip() {
         if (faceUp) {
             currentImage = back;
@@ -114,24 +126,26 @@ public class Card extends JComponent {
         // System.out.println("currentImage: " + currentImage.getWidth() + ", "
         // + currentImage.getHeight());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(500); // pause so you can see the flip
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
-        }
-        // setPreferredSize(new Dimension(currentImage.getWidth(null),
-        // currentImage.getHeight(null)));
+        } // end catch
         faceUp = !faceUp;
-        repaint();
+        repaint(); // update the screen
     } // end method
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.swing.JComponent#getPreferredSize()
+     */
     public Dimension getPreferredSize() {
         if (currentImage == null) {
-            return new Dimension(100, 100);
+            return new Dimension(72, 96);
         } else {
             return new Dimension(currentImage.getWidth(null), currentImage.getHeight(null));
         }
-    }
+    } // end method
 
     /*
      * (non-Javadoc)
@@ -142,12 +156,15 @@ public class Card extends JComponent {
         return "" + rank + suit;
     } // end method
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     */
     protected void paintComponent(Graphics graphics) {
         super.paintComponents(graphics);
         Graphics g = graphics.create();
         g.drawImage(currentImage, 0, 0, null);
-        // g.drawImage(currentImage, 0, 0, currentImage.getWidth(null),
-        // currentImage.getHeight(null), null);
         g.dispose();
     } // end method
 } // end class
