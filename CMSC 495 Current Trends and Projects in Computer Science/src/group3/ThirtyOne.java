4121c3eb131c1008464b57fbae9c33643217e530
Patrick Smith <warlordpat@hotmail.com>
2016-09-10 11:31:47 -0400

    merged updated ThirtyOne

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/ThirtyOne.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/ThirtyOne.java
@@ -13,7 +13,6 @@ import java.lang.*;
 public class ThirtyOne {
     private Deck deck;
     private Hand ai, player;
-    JTextArea jtaHand;
     private static Map<Rank, Integer> values = new HashMap<>();
     static {
         values.put(Rank.ACE, 1);
@@ -30,12 +29,17 @@ public class ThirtyOne {
         values.put(Rank.QUEEN, 10);
         values.put(Rank.KING, 10);
     }
+    private Card playerCard1, playerCard2, playerCard3, aiCard1, aiCard2, aiCard3;
+    private ImageIcon playerCardImage1, playerCardImage2, playerCardImage3;
+    private JButton jbPlayerCard1, jbPlayerCard2, jbPlayerCard3;
+    private int player31;
+    private int ai31;
     public ThirtyOne() {
+        
         deck = new Deck();
         deck.shuffle();
         ai = new Hand();
         player = new Hand();
-        jtaHand = new JTextArea();
     }
     public void begin() {
         gui();
@@ -49,32 +53,81 @@ public class ThirtyOne {
         }
         ai = new Hand();
         player = new Hand();
-        player.addCard(deck.deal());
-        player.addCard(deck.deal());
-        player.addCard(deck.deal());
-        ai.addCard(deck.deal());
-        ai.addCard(deck.deal());
-        ai.addCard(deck.deal());
-        jtaHand.setText(display());
+        playerCard1 = deck.deal();
+        playerCard2 = deck.deal();
+        playerCard3 = deck.deal();
+        player.addCard(playerCard1);
+        player.addCard(playerCard2);
+        player.addCard(playerCard3);
+        playerCardImage1 = new ImageIcon(playerCard1.front);
+        playerCardImage2 = new ImageIcon(playerCard2.front);
+        playerCardImage3 = new ImageIcon(playerCard3.front);
+        jbPlayerCard1.setIcon(playerCardImage1);
+        jbPlayerCard2.setIcon(playerCardImage2);
+        jbPlayerCard3.setIcon(playerCardImage3);
+        aiCard1 = deck.deal();
+        aiCard2 = deck.deal();
+        aiCard3 = deck.deal();
+        ai.addCard(aiCard1);
+        ai.addCard(aiCard2);
+        ai.addCard(aiCard3);
     }
     public void gui() {
         Random rand = new Random();
         JFrame frame = new JFrame("Thirty-One");
         frame.setVisible(true);
+        frame.setSize(900, 900);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        menuBar.add(menu);
+        JMenuItem menuItem = new JMenuItem("New Game");
+        JMenuItem menuItemD = new JMenuItem("How to Play");
+        JMenuItem menuItem1 = new JMenuItem("High Scores");
+        JMenuItem menuItem2 = new JMenuItem("Return to main menu");
+        menu.add(menuItem);
+        menu.add(menuItemD);
+        menu.add(menuItem1);
+        menu.add(menuItem2);
+        menuItem.addActionListener(ae -> {
+            frame.dispose();
+            deck = new Deck();
+            deck.shuffle();
+            begin();
+        });
+        menuItemD.addActionListener(ae -> {
+            Directions();
+        });
+        menuItem1.addActionListener(ae -> {
+            //add in High Score code
+        });
+        menuItem2.addActionListener(ae -> {
+            frame.dispose();
+        });
+        frame.setJMenuBar(menuBar);
         JSplitPane splitPaneHorizontal = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         JPanel TopPanel = new JPanel(new BorderLayout());
-        JPanel BottomPanel = new JPanel();
-        jtaHand.setText(display());
-        BottomPanel.add(jtaHand);
+        JPanel BottomPanel = new JPanel(new BorderLayout());
         Card Card1 = deck.deal();
         Card Card2 = deck.deal();
         Card Card3 = deck.deal();
+        ImageIcon back = new ImageIcon(Card1.back);
+        jbPlayerCard1 = new JButton(back);
+        jbPlayerCard2 = new JButton(back);
+        jbPlayerCard3 = new JButton(back);
+        BottomPanel.add(jbPlayerCard1, BorderLayout.LINE_START);
+        BottomPanel.add(jbPlayerCard2, BorderLayout.CENTER);
+        BottomPanel.add(jbPlayerCard3, BorderLayout.LINE_END);
         int iCard1 = values.get(Card1.getRank());
         int iCard2 = values.get(Card2.getRank());
         int iCard3 = values.get(Card3.getRank());
-        JButton jbCard1 = new JButton(Card1.toString());
-        JButton jbCard2 = new JButton(Card2.toString());
-        JButton jbCard3 = new JButton(Card3.toString());
+        ImageIcon CenterCard1 = new ImageIcon(Card1.front);
+        ImageIcon CenterCard2 = new ImageIcon(Card2.front);
+        ImageIcon CenterCard3 = new ImageIcon(Card3.front);
+        JButton jbCard1 = new JButton(CenterCard1);
+        JButton jbCard2 = new JButton(CenterCard2);
+        JButton jbCard3 = new JButton(CenterCard3);
         if ((rand.nextInt(2)+1) == 1)
         {
             TopPanel.add(jbCard1, BorderLayout.LINE_START);
@@ -110,8 +163,11 @@ public class ThirtyOne {
             {
                 player.addCard(Card1);
                 TopPanel.remove(jbCard1);
-                jtaHand.setText(display());
-                if (player.total() > ai.total())
+                player31 = 31 - player.total();
+                player31 = Math.abs(player31);
+                ai31 = 31 - ai.total();
+                ai31 = Math.abs(ai31);
+                if (player31 < ai31)
                 {
                     Object[] options = {"Yes", "No"};
                     int n = JOptionPane.showOptionDialog(frame, ("You win " + player.total() + " to " + ai.total() + "!\nWould you like to play again?"), "You Win!", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
@@ -127,7 +183,7 @@ public class ThirtyOne {
                         frame.dispose();
                     }
                 }
-                else if (player.total() == ai.total())
+                else if (player31 == ai31)
                 {
                     Object[] options = {"Yes", "No"};
                     int n = JOptionPane.showOptionDialog(frame, ("You tied at " + player.total() + " to " + ai.total() + "!\nWould you like to play again?"), "You Tie", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
@@ -170,7 +226,6 @@ public class ThirtyOne {
             {
                 player.addCard(Card2);
                 TopPanel.remove(jbCard2);
-                jtaHand.setText(display());
                 if (player.total() > ai.total())
                 {
                     Object[] options = {"Yes", "No"};
@@ -230,7 +285,6 @@ public class ThirtyOne {
             {
                 player.addCard(Card3);
                 TopPanel.remove(jbCard3);
-                jtaHand.setText(display());
                 if (player.total() > ai.total())
                 {
                     Object[] options = {"Yes", "No"};
@@ -285,12 +339,24 @@ public class ThirtyOne {
                 JOptionPane.showMessageDialog(null, "You already have 4 cards in your hand");
             }
         });
-        splitPaneHorizontal.setTopComponent(TopPanel);
-        splitPaneHorizontal.setBottomComponent(BottomPanel);
+        JSplitPane split1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        JSplitPane split2 = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        JLabel jlCenter = new JLabel("Center Cards");
+        JLabel jlPlayer = new JLabel("Player Hand");
+        JPanel centerPanel = new JPanel();
+        JPanel playerPanel = new JPanel();
+        centerPanel.add(jlCenter);
+        playerPanel.add(jlPlayer);
+        split1.setTopComponent(centerPanel);
+        split1.setBottomComponent(TopPanel);
+        split2.setTopComponent(playerPanel);
+        split2.setBottomComponent(BottomPanel);
+        splitPaneHorizontal.setTopComponent(split1);
+        splitPaneHorizontal.setBottomComponent(split2);
         frame.add(splitPaneHorizontal);
-        frame.setSize(500, 300);
+        frame.pack();
     }
-    public String display() {
-        return player.toString();
+    public void Directions() {
+        JOptionPane.showMessageDialog(null, "The goal of the game is to get the closest to 31. Whichever player is closer wins. Click one of the center cards to add it to your hand.");
     }
 }
