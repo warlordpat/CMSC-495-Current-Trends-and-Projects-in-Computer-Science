bfff1321cee894cbf0412ee786e30252bcd66348
Patrick Smith <warlordpat@hotmail.com>
2016-09-21 21:53:33 -0400

    Cleaned up and commented methods.  Refactored for style.

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/War.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/War.java
@@ -1,51 +1,147 @@
+// File: War.java
+// Author: Patrick Smith
+// Date: Sep 14, 2016
+// Course: CMSC 495
+// Assignment: Final Project, Group 3
+// Platform: Win10 x64 Java build 1.8.0_102
+// Purpose: implements a graphical game of War.
+
 package group3;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.net.URL;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
+/**
+ * This class implements the GUI and game logic of the card game War.
+ *
+ * @author Patrick Smith
+ * @version 1.0
+ * @since Sep 21, 2016
+ */
 public class War {
-
-    URL imageURL = this.getClass().getClassLoader().getResource("images/cardBack.png");
-    ImageIcon cardBacks = new ImageIcon(imageURL);
-    private Deck deck;
+    /**
+     * The number of columns on the game board.
+     */
+    private static final int BOARD_COLS = 3;
+    /**
+     * The width of a card.
+     */
+    private static final int CARD_WIDTH = 106;
+    /**
+     * The height of a card.
+     */
+    private static final int CARD_HEIGHT = 144;
+    /**
+     * The width of the remaining cards label.
+     */
+    private static final int REMAIN_WIDTH = 37;
+    /**
+     * The height of the remaining cards label.
+     */
+    private static final int REMAIN_HEIGHT = 33;
+    /**
+     * Size of remaining cards font.
+     */
+    private static final int REMAIN_FONT_SIZE = 30;
+    /**
+     * Keeps track of how many battles the game takes.
+     */
     private int playCount = 0;
+    /**
+     * 
+     */
     private int warIndex = 0;
-    private Hand ai, player;
-    private Card playerCard, aiCard;
-    JTextArea jta;
-    JFrame frame;
-    WarFrame warFrame;
-    JPanel panel;
-    JPanel gameBoard;
-
-    JLabel playerDiscardLabel;
-    JLabel aiDiscardLabel;
-    JLabel blankLabel1;
-    JLabel blankLabel2;
-    JLabel blankLabel3;
-    boolean bNew;
+    /**
+     * The player's cards.
+     */
+    private Hand player;
+    /**
+     * The computer's cards.
+     */
+    private Hand ai;
+    /**
+     * The frame that hold the War GUI.
+     */
+    private JFrame frame;
+    /**
+     * Additional Frame to hold the Wars.
+     */
+    private WarFrame warFrame;
+    /**
+     * The panel that holds the game elements.
+     */
+    private JPanel panel;
+    /**
+     * Interior panel used to display the active player discard piles.
+     */
+    private JPanel gameBoard;
+    /**
+     * Label used to display the player discard pile.
+     */
+    private JLabel playerDiscardLabel;
+    /**
+     * Label used to display the computer discard pile.
+     */
+    private JLabel aiDiscardLabel;
+    /**
+     * Empty label at the center of the GUI. Used to space out the game GUI.
+     */
+    private JLabel blankLabel1;
+    /**
+     * 
+     */
+    private boolean bNew;
+    /**
+     * The remaining cards in the computer hand.
+     */
     private JLabel handSizeAI;
+    /**
+     * The remaining cards in the player hand.
+     */
     private JLabel handSizePlayer;
+    /**
+     * 
+     */
     private boolean gameOver;
 
+    /**
+     * Constructs a new War object with appropriate graphics and initializes
+     * member fields.
+     */
     public War() {
         frame = new JFrame("War!");
         warFrame = new WarFrame("It's War!");
         panel = new JPanel(new BorderLayout());
-        gameBoard = new JPanel(new GridLayout(0, 3, 2, 2));
+        gameBoard = new JPanel(new GridLayout(0, BOARD_COLS, 2, 2));
         playerDiscardLabel = new JLabel();
         playerDiscardLabel.setHorizontalAlignment(SwingConstants.CENTER);
         aiDiscardLabel = new JLabel();
         aiDiscardLabel.setHorizontalAlignment(SwingConstants.CENTER);
         blankLabel1 = new JLabel("  ");
-        blankLabel2 = new JLabel("  ");
-        blankLabel3 = new JLabel("  ");
         gameOver = false;
-    }
+    } // end constructor
 
-    public void begin() {
-        deck = new Deck();
+    /**
+     * Creates a new deck, shuffles it, and deals all the cards to the player
+     * and the computer. Builds the GUI and starts the game.
+     */
+    public final void begin() {
+        Deck deck = new Deck();
         deck.shuffle();
         ai = new Hand();
         player = new Hand();
@@ -58,20 +154,27 @@ public class War {
                 ai.addCard(temp);
             } // end else
         } // end if
-        jta = new JTextArea(50, 50);
         bNew = false;
         gui();
         playCount = 0;
-    }
+    } // end method
 
-    public static void main(String[] args) {
+    /**
+     * Runs a test of the War class.
+     *
+     * @param args
+     *            Not used
+     */
+    public static void main(final String[] args) {
         War test = new War();
         test.begin();
-    }
+    } // end main
 
-    public void gui() {
+    /**
+     * Builds the game GUI.
+     */
+    public final void gui() {
         frame.setVisible(true);
-        frame.setSize(900, 900);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         JMenuBar menuBar = new JMenuBar();
@@ -106,27 +209,29 @@ public class War {
             frame.dispose();
         });
         JLayeredPane layeredPanePlayer = new JLayeredPane();
+        URL imageURL = this.getClass().getClassLoader().getResource("images/cardBack.png");
+        ImageIcon cardBacks = new ImageIcon(imageURL);
         JLabel backLabel = new JLabel(cardBacks, JLabel.CENTER);
-        backLabel.setSize(106, 144);
+        backLabel.setSize(CARD_WIDTH, CARD_HEIGHT);
         layeredPanePlayer.setPreferredSize(backLabel.getPreferredSize());
-        handSizePlayer = new JLabel("26");
-        handSizePlayer.setFont(new Font("Tahoma", Font.PLAIN, 30));
+        handSizePlayer = new JLabel(String.valueOf(player.handSize()));
+        handSizePlayer.setFont(new Font("Tahoma", Font.PLAIN, REMAIN_FONT_SIZE));
         handSizePlayer.setHorizontalAlignment(SwingConstants.CENTER);
-        handSizePlayer.setLocation(69, 0);
-        handSizePlayer.setSize(37, 33);
+        handSizePlayer.setLocation(CARD_WIDTH - REMAIN_WIDTH, 0);
+        handSizePlayer.setSize(REMAIN_WIDTH, REMAIN_HEIGHT);
         handSizePlayer.setBackground(Color.WHITE);
         layeredPanePlayer.add(handSizePlayer, new Integer(1));
         layeredPanePlayer.add(backLabel, new Integer(0));
 
         JLabel backLabel2 = new JLabel(cardBacks, JLabel.CENTER);
         JLayeredPane layeredPaneAI = new JLayeredPane();
-        backLabel2.setSize(106, 144);
+        backLabel2.setSize(CARD_WIDTH, CARD_HEIGHT);
         layeredPaneAI.setPreferredSize(backLabel.getPreferredSize());
-        handSizeAI = new JLabel("26");
-        handSizeAI.setFont(new Font("Tahoma", Font.PLAIN, 30));
+        handSizeAI = new JLabel(String.valueOf(ai.handSize()));
+        handSizeAI.setFont(new Font("Tahoma", Font.PLAIN, REMAIN_FONT_SIZE));
         handSizeAI.setHorizontalAlignment(SwingConstants.CENTER);
-        handSizeAI.setLocation(0, 0);
-        handSizeAI.setSize(37, 33);
+        // handSizeAI.setLocation(0, 0);
+        handSizeAI.setSize(REMAIN_WIDTH, REMAIN_HEIGHT);
         handSizeAI.setBackground(Color.WHITE);
         layeredPaneAI.add(handSizeAI, new Integer(1));
         layeredPaneAI.add(backLabel2, new Integer(0));
@@ -144,12 +249,18 @@ public class War {
         panel.add(jbOne, BorderLayout.SOUTH);
         frame.getContentPane().add(panel);
         frame.pack();
-    }
+    } // end method
 
-    public void battle() {
+    /**
+     * Draws and compares cards from each player. If the cards are the same
+     * rank, starts a war. Returns the cards to the hand of the player with the
+     * higher ranking card.
+     */
+    public final void battle() {
         Card playerCard = player.removeCard();
         Card aiCard = ai.removeCard();
         playerDiscardLabel.setIcon(new ImageIcon(playerCard.front));
+//        playerDiscardLabel.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
         aiDiscardLabel.setIcon(new ImageIcon(aiCard.front));
         if (playerCard.getRank().compareTo(aiCard.getRank()) > 0) {
             // player card has a higher rank than ai card
@@ -183,7 +294,7 @@ public class War {
                 ai.addCard(aiCard);
                 updateHandSizeDisplay();
                 return;
-            }else if (ai.handSize() == 0) {
+            } else if (ai.handSize() == 0) {
                 blankLabel1.setText("Game Over/nYou win!");
                 gameOver = true;
                 player.addCard(playerCard);
@@ -203,15 +314,12 @@ public class War {
     } // end method
 
     /**
-     * 
+     * Starts a war when the player and the AI have the same rank card.
+     *
+     * @return True, if the player won the war
      */
     private boolean war() {
         System.out.println("Starting hands: player: " + player.handSize() + " ai: " + ai.handSize());
-        // blankLabel1.setText("You lost this fight!");
-        // warLabel1.setText("YOU ");
-        // warLabel2.setText("LOST ");
-        // warLabel3.setText("THIS ");
-        // warLabel4.setText("BATTLE!");
         blankLabel1.setText("WAR!");
         boolean whoWon = warFrame.setupGUI(player, ai, 1);
         System.out.println("Ending hands: player: " + player.handSize() + " ai: " + ai.handSize());
@@ -219,15 +327,20 @@ public class War {
     } // end method
 
     /**
-     * 
+     * Updates the players' hand sizes in the GUI.
      */
     private void updateHandSizeDisplay() {
         handSizePlayer.setText(Integer.toString(player.handSize()));
         handSizeAI.setText(Integer.toString(ai.handSize()));
     }
 
-    public void directions() {
+    /**
+     * Shows the game play directions to the user.
+     */
+    public final void directions() {
         JOptionPane.showMessageDialog(null,
-                "The player with the higher card wins. \nIn the event of a tie, each player offers \nthe top 3 cards of their decks, and \nthen the player with the highest \n4th card wins the whole pile.");
-    }
-}
+                "The player with the higher card wins. " + "\nIn the event of a tie, each player offers "
+                        + "\nthe top 3 cards of their decks, and " + "\nthen the player with the highest "
+                        + "\n4th card wins the whole pile.");
+    } // end method
+} // end class
