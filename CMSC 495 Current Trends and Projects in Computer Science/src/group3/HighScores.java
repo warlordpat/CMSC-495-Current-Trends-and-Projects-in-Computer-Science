fa4ef8b76d4d2b6638b758db0022451f34710954
Patrick Smith <warlordpat@hotmail.com>
2016-09-24 15:23:29 -0400

    Updated comments

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/HighScores.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/HighScores.java
@@ -2,9 +2,9 @@
 // Author: Patrick Smith
 // Date: Sep 14, 2016
 // Course: CMSC 495
-// Assignment: TODO
+// Assignment: Final Project, Group 3
 // Platform: Win10 x64 Java build 1.8.0_102
-// Purpose: TODO
+// Purpose: Holds high scores for use in the CGS
 package group3;
 
 import java.io.Serializable;
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
+ * The high score list for use in the CGS.
+ *
  * @author Patrick Smith
  * @version 1.0
  * @since Sep 14, 2016
@@ -19,16 +21,33 @@ import java.util.Collections;
 public class HighScores implements Serializable {
 
     /**
-     * 
+     * The maximum number of high scores to save in the list.
+     */
+    private static final int MAX_SCORES = 10;
+    /**
+     * The serial number for serialization.
      */
     private static final long serialVersionUID = 7079026133184545339L;
+    /**
+     * The high scores.
+     */
     private ArrayList<HighScore> highscores;
 
+    /**
+     * Creates a new empty high score list.
+     */
     HighScores() {
-        highscores = new ArrayList<>(10);
+        highscores = new ArrayList<>(MAX_SCORES);
     }
 
-    boolean add(HighScore score) {
+    /**
+     * Adds a score to the high score list.
+     *
+     * @param score
+     *            the score to add to the list
+     * @return true, if the score was a high score and added
+     */
+    final boolean add(final HighScore score) {
         if (highscores.isEmpty()) {
             // only need to save the current highscore
             highscores.add(score);
@@ -36,7 +55,7 @@ public class HighScores implements Serializable {
         } else {
             HighScore lowestHighScore = highscores.get(highscores.size() - 1);
             if (isHighScore(score.getScore())) {
-                if (highscores.size() == 10) {
+                if (highscores.size() == MAX_SCORES) {
                     highscores.remove(lowestHighScore);
                 } // end if
                   // Save new score
@@ -48,7 +67,13 @@ public class HighScores implements Serializable {
         }
     } // end method
 
-    public String toString() {
+    /**
+     * Formats the high scores as a string.
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public final String toString() {
         String outputScores = "High Scores\n";
         if (highscores.isEmpty()) {
             return outputScores + "\nNo High Scores Yet!";
@@ -66,16 +91,12 @@ public class HighScores implements Serializable {
      *            the score to check
      * @return true, if the score qualifies as a high score
      */
-    public boolean isHighScore(double score) {
+    public final boolean isHighScore(final double score) {
         if (highscores.size() == 0) {
             return true;
         }
         HighScore lowestHighScore = highscores.get(highscores.size() - 1);
-        if (lowestHighScore.getScore() < score || highscores.size() < 10) {
-            return true;
-        } else {
-            return false;
-        }
+        return lowestHighScore.getScore() < score || highscores.size() < MAX_SCORES;
 
     }
 } // end class
