2a6d431852a0a286b188f30c12a04726fb681ad1
Patrick Smith <warlordpat@hotmail.com>
2016-09-24 15:21:39 -0400

    Updated comments, removed deprecated code

--- CMSC 495 Current Trends and Projects in Computer Science/src/group3/Deck.java
+++ CMSC 495 Current Trends and Projects in Computer Science/src/group3/Deck.java
@@ -1,5 +1,13 @@
+// File: Deck.java
+// Author: Patrick Smith
+// Date: Sep 5, 2016
+// Course: CMSC 495
+// Assignment: Final Project, Group 3
+// Platform: Win10 x64 Java build 1.8.0_102
+// Purpose: implements a graphical Deck for card games.
 package group3;
 
+//TODO Remove Magic Numbers
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -38,18 +46,9 @@ public class Deck extends JLabel {
      */
     private Random rand;
     /**
-     * The image of the back of the card
+     * The image of the back of the card.
      */
-    transient private BufferedImage cardBack;
-
-    // below fields will be removed once games convert to functionality in the
-    // Hand class.
-//    static ImageIcon[] cardIcon = new ImageIcon[52];
-//    static int[] refArray = new int[52];
-//    private ArrayList<Integer> playerHand = new ArrayList<>();
-//    private ArrayList<Integer> aiHand = new ArrayList<>();
-//    private ArrayList<ImageIcon> playerCards = new ArrayList<>();
-//    private ArrayList<ImageIcon> AICards = new ArrayList<>();
+    private transient BufferedImage cardBack;
 
     /**
      * Generate a new full, un-shuffled deck. Loads a card back to represent the
@@ -72,22 +71,47 @@ public class Deck extends JLabel {
         cardBack = new Card(Rank.ACE, Suit.CLUB).getBacks().getSubimage(72, 0, 72, 96);
     } // end method
 
-    private void writeObject(ObjectOutputStream out) throws IOException {
+    /**
+     * Serializes the Deck.
+     *
+     * See readObject() and writeObject() in JComponent for more information
+     * about serialization in Swing.
+     *
+     * @param out
+     *            the <code>ObjectOutputStream</code> in which to write
+     * @throws IOException
+     *             if I/O errors occur while writing to the OutputStream
+     */
+    private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
         ImageIO.write(cardBack, "png", out);
     } // end method
-    
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+
+    /**
+     * Reads back in the Deck from serialization.
+     *
+     * See readObject() and writeObject() in JComponent for more information
+     * about serialization in Swing.
+     *
+     * @param in
+     *            the <code>ObjectInputStream</code> from which to read
+     * @throws IOException
+     *             if an I/O error occurs
+     * @throws ClassNotFoundException
+     *             if the class of a serialized object could not be found
+     */
+    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         cardBack = ImageIO.read(in);
     } // end method
-    
-    
+
     /**
      * Shuffles the deck randomly.
      */
-    void shuffle() {
-        System.out.println("Starting shuffle");
+    final void shuffle() {
+        if (group3.DEBUGGING) {
+            System.out.println("Starting shuffle");
+        }
         List<Card> temp = cards;
         cards = new ArrayList<>();
         // Randomly draws a card out of the deck until the temp deck is empty
@@ -97,277 +121,48 @@ public class Deck extends JLabel {
             Card randomCard = temp.remove(random);
             cards.add(randomCard);
         } // end while
-        System.out.println("end of good shuffle");
-        // Deprecated shuffle and image load code. Left in for now to not break
-        // anything.
-        // {
-        // for (int j = 1; j <= 52; j++) {
-        //
-        // int position = 0;
-        // do {
-        // position = (int) (Math.random() * 52);
-        // } while (cardIcon[position] != null);
-        //
-        // URL cardURL = this.getClass().getClassLoader().getResource("images/"
-        // + j + ".png");
-        // cardIcon[position] = new ImageIcon(cardURL);
-        // }
-        // String refPath = cardIcon[0].toString();
-        // String filePath = refPath.substring(0, refPath.lastIndexOf("/"));
-        //
-        // for (int ref2 = 0; ref2 < 52; ref2++) {
-        //
-        // for (int ref = 1; ref <= 52; ref++) {
-        //
-        // if (cardIcon[ref2].toString().equals(filePath + "/" + ref + ".png"))
-        // {
-        //
-        // if (ref == 1 || ref == 14 || ref == 27 || ref == 40) {
-        // refArray[ref2] = 14;
-        // }
-        // if (ref == 2 || ref == 15 || ref == 28 || ref == 41) {
-        // refArray[ref2] = 2;
-        // }
-        // if (ref == 3 || ref == 16 || ref == 29 || ref == 42) {
-        // refArray[ref2] = 3;
-        // }
-        // if (ref == 4 || ref == 17 || ref == 30 || ref == 43) {
-        // refArray[ref2] = 4;
-        // }
-        // if (ref == 5 || ref == 18 || ref == 31 || ref == 44) {
-        // refArray[ref2] = 5;
-        // }
-        // if (ref == 6 || ref == 19 || ref == 32 || ref == 45) {
-        // refArray[ref2] = 6;
-        // }
-        // if (ref == 7 || ref == 20 || ref == 33 || ref == 46) {
-        // refArray[ref2] = 7;
-        // }
-        // if (ref == 8 || ref == 21 || ref == 34 || ref == 47) {
-        // refArray[ref2] = 8;
-        // }
-        // if (ref == 9 || ref == 22 || ref == 35 || ref == 48) {
-        // refArray[ref2] = 9;
-        // }
-        // if (ref == 10 || ref == 23 || ref == 36 || ref == 49) {
-        // refArray[ref2] = 10;
-        // }
-        // if (ref == 11 || ref == 24 || ref == 37 || ref == 50) {
-        // refArray[ref2] = 11;
-        // }
-        // if (ref == 12 || ref == 25 || ref == 38 || ref == 51) {
-        // refArray[ref2] = 12;
-        // }
-        // if (ref == 13 || ref == 26 || ref == 39 || ref == 52) {
-        // refArray[ref2] = 13;
-        // }
-        // }
-        // }
-        //
-        // }
-        //
-        // for (int iNum = 0; iNum <= 26; iNum++) {
-        // playerHand.add(deal(iNum));
-        // playerCards.add(returnCard(iNum));
-        // // playerRefArray.add(deck.deal(iNum));
-        // aiHand.add(deal(iNum + 1));
-        // AICards.add(returnCard(iNum + 1));
-        // // AIRefArray.add(deck.deal(iNum + 1));
-        // // player.addCard(deck.deal());
-        // // ai.addCard(deck.deal());
-        // }
-        // } // end deprecated block
-        System.out.println("shuffle complete");
+        if (group3.DEBUGGING) {
+            System.out.println("end of good shuffle");
+            System.out.println("shuffle complete");
+        }
     } // end method
 
     /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param tempPlayerCard
-//     *            image of a card
-//     */
-//    @Deprecated
-//    void addPlayerCards(ImageIcon tempPlayerCard) {
-//        playerCards.add(tempPlayerCard);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param cardValue
-//     *            the value of a card
-//     */
-//    @Deprecated
-//    void addPlayerHand(int cardValue) {
-//        playerHand.add(cardValue);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to remove
-//     */
-//    @Deprecated
-//    void removePlayerCards(int index) {
-//        playerCards.remove(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to remove
-//     */
-//    @Deprecated
-//    void removePlayerHand(int index) {
-//        playerHand.remove(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to get
-//     * @return
-//     */
-//    @Deprecated
-//    ImageIcon getPlayerCards(int index) {
-//        return playerCards.get(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to get
-//     * @return
-//     */
-//    @Deprecated
-//    int getPlayerHand(int index) {
-//        return playerHand.get(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param tempPlayerCard
-//     */
-//    @Deprecated
-//    void addAICards(ImageIcon tempPlayerCard) {
-//        AICards.add(tempPlayerCard);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param cardValue
-//     */
-//    @Deprecated
-//    void addAIHand(int cardValue) {
-//        aiHand.add(cardValue);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     */
-//    @Deprecated
-//    void removeAICards(int index) {
-//        AICards.remove(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to remove
-//     */
-//    @Deprecated
-//    void removeAIHand(int index) {
-//        aiHand.remove(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to get
-//     * @return
-//     */
-//    @Deprecated
-//    int getAIHand(int index) {
-//        return aiHand.get(index);
-//    }
-//
-//    /**
-//     * Functionality will be moved into Hand class.
-//     * 
-//     * @param index
-//     *            the index of the card to get
-//     * @return
-//     */
-//    @Deprecated
-//    ImageIcon getAICards(int index) {
-//        return AICards.get(index);
-//    }
-//
-//    /**
-//     * Deals a card from the deck. Functionality will be moved into Hand class.
-//     * 
-//     * @return a Card from the top of the deck
-//     */
-//    @Deprecated
-//    int deal(int index) {
-//        return refArray[index];
-//    } // end method
-
-    /**
      * Deals a card from the deck.
-     * 
+     *
      * @return a Card from the top of the deck
      */
-    Card deal() {
+    final Card deal() {
         return cards.remove(0);
     } // end method
 
     /**
-//     * Returns a card image of the card at the given array location.
-//     * 
-//     * @param index
-//     *            the array index
-//     * @return an image of the card at that index
-//     */
-//    @Deprecated
-//    ImageIcon returnCard(int index) {
-//        return cardIcon[index];
-//    }
-
-    /*
-     * (non-Javadoc)
-     * 
+     * Converts a Deck to a String representation.
+     *
      * @see java.lang.Object#toString()
+     * @return A String representation of a Deck
      */
-    public String toString() {
+    public final String toString() {
         return cards.toString();
     } // end method
 
     /**
      * Checks the current size of the deck.
-     * 
+     *
      * @return the current size of the deck
      */
-    public int deckSize() {
+    public final int deckSize() {
         return cards.size();
     } // end method
 
-    /*
-     * (non-Javadoc)
-     * 
+    /**
+     * Draws the hand based on the size of the Hand.
+     *
      * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     * @param graphics
+     *            the Graphics object to protect
      */
-    protected void paintComponent(Graphics graphics) {
+    protected final void paintComponent(final Graphics graphics) {
         Graphics2D g = (Graphics2D) graphics.create();
         if (!isEmpty) {
             g.drawImage(cardBack, 0, 0, null);
